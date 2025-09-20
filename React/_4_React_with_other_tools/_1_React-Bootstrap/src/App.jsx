79038b162587dfda57bef8a1164d1719df6257ee
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2025-09-20 20:14:29 +0530

    React Bootstrape Added

---
+++ React/_4_React_with_other_tools/_1_React-Bootstrap/src/App.jsx
@@ -0,0 +1,12 @@
+import Buttons from './components/Buttons'
+import Badges from './components/Badges'
+function App() {
+  return (
+    <div>
+      <Buttons/>
+      <Badges/>
+    </div>
+  )
+}
+
+export default App
