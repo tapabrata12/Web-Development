3cf04d27769c8a3fea5be72fe0936108d8734a8d
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2025-11-02 12:38:41 +0530

    feat: Implement POST request handling in HTTP server with JSON response

--- Backend/_4_HttpServer/sever.js
+++ Backend/_4_HttpServer/sever.js
@@ -1,19 +1,19 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     // Now I have to write the logic to accept the request and send the responce
-    if (req.url === '/' && req.method === 'GET'){
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.end('Hello, world!');
-    }else if(req.url === '/about' && req.method === 'GET'){
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.end('Hello from the About page');
-    }else{
-        res.writeHead(404, {'content-type': 'text/plain'});
-        res.end('Sorry, page not found!');
+    if (req.url === "/" && req.method === "GET") {
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.end("Hello, world!");
+    } else if (req.url === "/about" && req.method === "GET") {
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.end("Hello from the About page");
+    }else {
+        res.writeHead(404, { "content-type": "text/plain" });
+        res.end("Sorry, page not found!");
     }
 });
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server is listening on Port http://localhost:${PORT}/`);
 });
