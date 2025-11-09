a84e9ca4d574187698da91ce9a3678c25ea8c76a
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2025-11-09 09:48:27 +0530

    feat: Enhance file system operations with append, rename, copy, delete, and directory management

--- Backend/_3_Modules/InbuildModules/FS.js
+++ Backend/_3_Modules/InbuildModules/FS.js
@@ -17,3 +17,54 @@ fs.readFile('_3_Modules/InbuildModules/data2.txt','utf-8',(err,data2)=>{
     if(err) throw err;
     console.log(data2);
 });
+
+// Kuch Extra Jor doo existing file me
+fs.appendFile('_3_Modules/InbuildModules/data2.txt',"\nAppend new line into Data 2 file",(err)=>{
+    if(err) throw err;
+    console.log("File Append Successfully");
+});
+
+// Rename File
+
+fs.rename('_3_Modules/InbuildModules/data2.txt',"_3_Modules/InbuildModules/data2_Renamed.txt",(err)=>{
+    if(err) throw err;
+    console.log("File renamed successfully");
+});
+
+// Make Directory
+fs.mkdir('_3_Modules/InbuildModules/copy',(err)=>{
+    if(err) throw err;
+    console.log('Directory created successfully');
+});
+
+// Copy file
+
+fs.copyFile('_3_Modules/InbuildModules/data1.txt','_3_Modules/InbuildModules/copy/data1_Copy.txt',(err)=>{
+    if (err) throw err;
+    console.log('Copied successfully');
+});
+
+// Delete File 
+
+fs.unlink('_3_Modules/InbuildModules/data1.txt',(err)=>{
+    if(err) throw err;
+    console.log('File deleted successfully');
+});
+
+fs.unlink("_3_Modules/InbuildModules/data2_Renamed.txt",(err)=>{
+    if(err) throw err;
+    console.log('File deleted successfully');
+});
+
+fs.unlink('_3_Modules/InbuildModules/copy/data1_Copy.txt',(err)=>{
+    if(err) throw err;
+    console.log('File deleted successfully');
+});
+
+
+
+// Remove Directory
+fs.rm('_3_Modules/InbuildModules/copy',(err)=>{
+    if(err) throw err;
+    console.log('Directory removed successfully');
+});
